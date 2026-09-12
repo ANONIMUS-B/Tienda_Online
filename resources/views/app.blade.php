97fe6150db9743fa8dbaea83b97d4d9e1948d283
ANONIMUS-B <ayalaromerojordanbrandon@gmail.com>
2026-09-12 13:04:28 -0500

    Cambio de Incono

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,8 +32,10 @@
         </style>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/images/brand/jbtechline-logo.png" type="image/png">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="/images/brand/jbtechline-logo.png">
 
         @fonts
 
@@ -41,6 +43,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         <x-inertia::head>
             <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'JBTECHLINE') }} - Tu aliado tecnológico</title>
         </x-inertia::head>
     </head>
     <body class="notranslate font-sans antialiased" translate="no">
